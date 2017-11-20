f9eafd89ea582fc2b4a03e7ffc66607ecb347140
emalppro@gmail.com <emalppro@gmail.com>
2017-11-20 22:20:46 +1100

    debugged everything

--- uacOfferReader.py
+++ uacOfferReader.py
@@ -1,18 +1,21 @@
 import mechanize
 from bs4 import BeautifulSoup
 
-offerSite = 'https://www32.uac.edu.au/offers'
 
+offerSite = 'https://www32.uac.edu.au/offers'
 br = mechanize.Browser()
+applicationNum = ""
+pin = ""
 
 
 def mainBrowsing():
 	br.open(offerSite)
 
 	br.select_form(nr=0)
+	
 
-	br.form['refNum'] = '' # the uac number
-	br.form['pin'] = '' # the uac pin
+	br.form['refNum'] = applicationNum # the uac number
+	br.form['pin'] = pin # the uac pin
 
 	br.submit()
 
@@ -22,11 +25,23 @@ def mainBrowsing():
 
 	offerData = soup.find_all('p')
 	
-	print(offerData[0]+ '\n')
-	print(offerData[1] + '\n')
-	print(offerData[2] + '\n')
-	print(offerData[3] + '\n')
-	print(offerData[4] + '\n')
+	print offerData[0]
+	print '\n' 
+	print offerData[1]
+	print '\n' 
+	print offerData[2]
+	print '\n' 
+	print offerData[3]
+	print '\n' 
+	print offerData[4]
 
 
 
+infoFile = open('cred.txt')
+	
+applicationNum = str(infoFile.readline())
+pin = str(infoFile.readline())
+
+infoFile.close()
+
+mainBrowsing()
