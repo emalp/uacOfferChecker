ce5de1e26e87ba41b574c5d97ed6d775dc1e00bd
Sushant <emalppro@gmail.com>
2017-11-21 09:17:28 +1100

    git conflict managed

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
 	desciption = 'Checks for your UAC undergraduate offers',
 	author = 'emalp',
 	install_requires = ['mechanize', 'bs4', 'lxml'],
-	install_requires = ['mechanize', 'bs4'],
 	zip_safe = False
 	)
 
