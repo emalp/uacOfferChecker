fe70799548adcd689924d6cca5b6f52f235eec19
emalppro@gmail.com <emalppro@gmail.com>
2017-11-20 22:37:36 +1100

    Little bit of exception handling done

--- uacOfferReader.py
+++ uacOfferReader.py
@@ -1,27 +1,29 @@
 import mechanize
 from bs4 import BeautifulSoup
-
+import sys
 
 offerSite = 'https://www32.uac.edu.au/offers'
 br = mechanize.Browser()
 applicationNum = ""
 pin = ""
 
-
 def mainBrowsing():
-	br.open(offerSite)
+	try:
+		br.open(offerSite)
 
-	br.select_form(nr=0)
-	
+		br.select_form(nr=0)	
 
-	br.form['refNum'] = applicationNum # the uac number
-	br.form['pin'] = pin # the uac pin
+		br.form['refNum'] = applicationNum # the uac number
+		br.form['pin'] = pin # the uac pin
 
-	br.submit()
+		br.submit()
 
 	# eveything's done, now print the output to the user in nice format
 
-	soup = BeautifulSoup(br.response().read(), 'lxml')
+		soup = BeautifulSoup(br.response().read(), 'lxml')
+	except err: 
+		print "Can't get to the website!"
+		sys.exit(1)
 
 	offerData = soup.find_all('p')
 	
@@ -35,13 +37,15 @@ def mainBrowsing():
 	print '\n' 
 	print offerData[4]
 
-
-
-infoFile = open('cred.txt')
+try:
+	infoFile = open('cred.txt')
 	
-applicationNum = str(infoFile.readline())
-pin = str(infoFile.readline())
+	applicationNum = str(infoFile.readline())
+	pin = str(infoFile.readline())
 
-infoFile.close()
+	infoFile.close()
+except filerr:
+	print 'Cannot open file "cred.txt"'
+	sys.exit(1)
 
 mainBrowsing()
